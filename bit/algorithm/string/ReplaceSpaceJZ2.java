290fd816872bfe7b4914eccdb6652f9bbcf3c746
wangzilong2019 <1428075063@qq.com>
2020-08-08 22:11:23 +0800

    字符串操作练习

---
+++ bit/algorithm/string/ReplaceSpaceJZ2.java
@@ -0,0 +1,41 @@
+package bit.algorithm.string;
+
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+import java.util.Stack;
+
+public class ReplaceSpaceJZ2 {
+    public static String replaceSpace(StringBuffer str) {
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+        //首先统计空格字符的个数
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                count++;
+            }
+        }
+        int new_length = str.length() + count*2;
+        int old_end = str.length() - 1;
+        int new_end = new_length - 1;
+        //设置新字符串大小防止越界
+        str.setLength(new_length);
+        while (old_end >= 0 && new_end >= 0) {
+            if (str.charAt(old_end) == ' ') {
+                str.setCharAt(new_end--, '0');
+                str.setCharAt(new_end--, '2');
+                str.setCharAt(new_end--, '%');
+                old_end--;
+            } else {
+                str.setCharAt(new_end--, str.charAt(old_end));
+                old_end--;
+            }
+        }
+        return str.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println( replaceSpace(new StringBuffer("We Are Happy")));
+    }
+}
